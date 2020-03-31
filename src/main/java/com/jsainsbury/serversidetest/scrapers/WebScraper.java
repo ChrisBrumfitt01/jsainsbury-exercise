db4e559b2887b51bd7f87739d661c83e473de2f7
ChrisBrumfitt01 <chrisbrumfitt@hotmail.co.uk>
2020-03-31 19:28:14 +0100

    Added WebScraper class for parsing a web page when given a URL

---
+++ src/main/java/com/jsainsbury/serversidetest/scrapers/WebScraper.java
@@ -0,0 +1,24 @@
+package com.jsainsbury.serversidetest.scrapers;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class WebScraper {
+
+    private static final Logger LOG = Logger.getLogger(WebScraper.class.getName());
+
+    public Document parseWebpage(String url) {
+        try {
+            return Jsoup.connect(url)
+                    .get();
+        } catch (IOException e) {
+            LOG.log(Level.SEVERE, String.format("Could not parse the URL: %s", url));
+            return null; //TODO: Replace with Exception handler
+        }
+    }
+
+}
